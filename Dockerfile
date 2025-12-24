d7fd55f91d69e60314c493acdd3637470409e341
bharat <bharatjogi.07@gmail.com>
2025-12-24 20:56:47 +0530

    code changes

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# ---------- Build Stage ----------
+FROM node:22-alpine AS builder
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm ci
+
+COPY . .
+RUN npm run build   # tsc → dist/
+
+# ---------- Runtime Stage ----------
+FROM node:22-alpine
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm ci --only=production
+
+COPY --from=builder /app/dist ./dist
+
+EXPOSE 3000
+
+CMD ["node", "dist/index.js"]
--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,18 @@ import type { Request, Response } from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Hello Bharat");
 });
 
 app.get("/health", (req: Request, res: Response) => {
   res.send("up and running!");
 });
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
